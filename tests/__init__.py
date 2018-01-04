77f70df4d56f7e89693a10f9be7463c99cf597de
freeh4cker <freeh4cker@gmail.com>
2018-01-04 14:57:18 +0100

    add utility to hide stdout, stderr

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 import os
 import unittest
-
+import sys
+from io import StringIO
+from contextlib import contextmanager
 
 class EmgTest(unittest.TestCase):
 
@@ -11,3 +13,23 @@ class EmgTest(unittest.TestCase):
         if not os.path.exists(data_path):
             raise FileNotFoundError("{} not found".format(data_path))
         return data_path
+
+
+    @contextmanager
+    def catch_output(self, out=False, err=False):
+        """
+        Catch stderr and stdout of the code running within this block.
+        """
+        old_out = sys.stdout
+        new_out = old_out
+        old_err = sys.stderr
+        new_err = old_err
+        if out:
+            new_out = StringIO()
+        if err:
+            new_err = StringIO()
+        try:
+            sys.stdout, sys.stderr = new_out, new_err
+            yield sys.stdout, sys.stderr
+        finally:
+            sys.stdout, sys.stderr = old_out, old_err
