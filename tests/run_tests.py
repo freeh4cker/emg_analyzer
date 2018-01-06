8fe6c2ed63f359c74e193587d3ea4ef6621bba71
Bertrand Néron <freeh4cker@gmail.com>
2018-01-06 23:38:05 +0100

    fix copyright date

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ##########################################################################
-# Copyright (c) 2017 Bertrand Néron. All rights reserved.                #
+# Copyright (c) 2017-2018 Bertrand Néron. All rights reserved.                #
 # Use of this source code is governed by a BSD-style license that can be #
 # found in the LICENSE file.                                             #
 ##########################################################################
--- tests/unit/test_emg_norm.py
+++ tests/unit/test_emg_norm.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# Copyright (c) 2017 Bertrand Néron. All rights reserved.                #
+# Copyright (c) 2017-2018 Bertrand Néron. All rights reserved.                #
 # Use of this source code is governed by a BSD-style license that can be #
 # found in the LICENSE file.                                             #
 ##########################################################################
