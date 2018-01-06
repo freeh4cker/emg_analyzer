70cafb543c36ae484147b6508f07b1b2423ea94f
Bertrand Néron <freeh4cker@gmail.com>
2018-01-06 23:29:48 +0100

    fix files headers

--- emg_analyzer/utils.py
+++ emg_analyzer/utils.py
@@ -1,6 +1,3 @@
-#! /usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 ##########################################################################
 # Copyright (c) 2017 Bertrand Néron. All rights reserved.                #
 # Use of this source code is governed by a BSD-style license that can be #
